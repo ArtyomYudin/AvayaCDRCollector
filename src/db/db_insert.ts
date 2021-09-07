c55c478fa006b467e04d394fa9aac3be9a6c23b3
Artyom Yudin <yudin.artyom@gmail.com>
2021-09-07 17:41:06 +0300

    fix up

--- src/db/db_insert.ts
+++ src/db/db_insert.ts
@@ -1,2 +1,2 @@
-export const insertCDREvent = `INSERT INTO voip_traffic (traffic_date, duration, calling_number, called_number, call_code, voip_key)
+export const insertCDREvent = `REPLACE INTO voip_traffic (traffic_date, duration, calling_number, called_number, call_code, voip_key)
         VALUES (?, ?, ?, ?, ?, ?)`;
