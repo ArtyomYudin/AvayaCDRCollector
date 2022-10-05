ff75f8555329f660a5f5eb8709ff6e4f3ef3ddd9
ArtyomYudin <yudin.artyom@gmail.com>
2022-10-05 11:48:56 +0300

    fixup 05102022

--- src/features/socket-api.ts
+++ src/features/socket-api.ts
@@ -10,14 +10,7 @@ export function socketParseMessage(dbPool: Pool, msg: Buffer): void {
 
     const convCallDate = `20${callDateArray?.[2]}-${callDateArray?.[1]}-${callDateArray?.[0]} ${callTimeArray?.[0]}:${callTimeArray?.[1]}:00`;
 
-    const cdrEventValue = [
-      convCallDate,
-      cdrEventValueArray[2],
-      cdrEventValueArray[5],
-      cdrEventValueArray[3],
-      cdrEventValueArray[4],
-      cdrEventValueArray[0] + cdrEventValueArray[1] + cdrEventValueArray[2] + cdrEventValueArray[5] + cdrEventValueArray[3],
-    ];
+    const cdrEventValue = [convCallDate, cdrEventValueArray[2], cdrEventValueArray[5], cdrEventValueArray[3], cdrEventValueArray[4]];
     logger.info(convCallDate);
     logger.info(`Data received from client: ${cdrEventValue}`);
     try {
--- src/shared/db/db_insert.ts
+++ src/shared/db/db_insert.ts
@@ -1,2 +1,2 @@
-export const insertCDREvent = `REPLACE INTO voip_traffic (traffic_date, duration, calling_number, called_number, call_code, voip_key)
-        VALUES (?, ?, ?, ?, ?, ?)`;
+export const insertCDREvent = `INSERT INTO avaya_cdr (date, duration, calling_number, called_number, call_code)
+        VALUES (?, ?, ?, ?, ?)`;
