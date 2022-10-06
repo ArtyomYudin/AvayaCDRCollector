4d128146698e6b88515117dda35481a211274770
ArtyomYudin <yudin.artyom@gmail.com>
2022-10-06 15:28:16 +0300

    fix up

--- src/features/socket-api.ts
+++ src/features/socket-api.ts
@@ -11,8 +11,8 @@ export function socketParseMessage(dbPool: Pool, msg: Buffer): void {
     const convCallDate = `20${callDateArray?.[2]}-${callDateArray?.[1]}-${callDateArray?.[0]} ${callTimeArray?.[0]}:${callTimeArray?.[1]}:00`;
 
     const cdrEventValue = [convCallDate, cdrEventValueArray[2], cdrEventValueArray[5], cdrEventValueArray[3], cdrEventValueArray[4]];
-    logger.info(convCallDate);
-    logger.info(`Data received from client: ${cdrEventValue}`);
+    // logger.info(convCallDate);
+    // logger.info(`Data received from client: ${cdrEventValue}`);
     try {
       dbPool.query(dbInsert.insertCDREvent, cdrEventValue);
     } catch (error) {
